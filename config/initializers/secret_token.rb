378ccbd6c8d3c428fb35d4dff8bafb1d029d5043
Zuppr <zuppr@yahoo.co.uk>
2010-10-31 12:28:04 +0000

    Initial commit for urlshortener3.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Urlshortener3::Application.config.secret_token = '98594f42544c69812e2ea8084213c3adfaac2216f41a0dce7485b7e3555a858b8e4cd3367fa441c2b5cf1b8eefbe22bab1e7d8d6cf51a20e4e46f6ec6565b3c8'
